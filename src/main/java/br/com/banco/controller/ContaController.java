dfe3ce965683c10450ee25946acdbdc82b8e6ce6
DESKTOP-E871I71\Maced <macedo-joao@live.com>
2023-07-16 01:55:33 -0300

    [CHANGE]Implement filter for transfers by date and operator name

--- src/main/java/br/com/banco/controller/ContaController.java
+++ src/main/java/br/com/banco/controller/ContaController.java
@@ -3,22 +3,30 @@ package br.com.banco.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.entity.Conta;
-import br.com.banco.repository.ContaRepository;
+import br.com.banco.service.ContaService;
 
 @RestController
-@RequestMapping("conta")
+@RequestMapping("/contas")
 public class ContaController {
-	@Autowired
-	private ContaRepository contaRepository;
 
-	@GetMapping
-	public List<Conta>findAll(){
-		return contaRepository.findAll();
+    @Autowired
+    private ContaService contaService;
+  
+    @GetMapping
+	public List<Conta> findAll(){
+		return contaService.findAll();
 	}
-	
+
+    @GetMapping("/{idConta}")
+    public ResponseEntity<Conta> obterContaPorId(@PathVariable("idConta") Long idConta) {
+        Conta conta = contaService.obterContaPorId(idConta);
+		return ResponseEntity.ok(conta);
+    }
 }
--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -5,37 +5,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.banco.entity.Transferencia;
-import br.com.banco.repository.TransferenciaRepository;
 import br.com.banco.service.TransferenciaService;
 
 @RestController
 @RequestMapping("transferencias")
 public class TransferenciaController {
 
-	@Autowired
-	private TransferenciaService transferenciaService;
     @Autowired
-    private TransferenciaRepository repository;
-    
+    private TransferenciaService transferenciaService;
     @GetMapping
-	public List<Transferencia> getAll(){
-		return repository.findAll();
-	}
-
-
-    @GetMapping("/por-data")
-    public List<Transferencia> obterTransacoesPorData(@RequestParam("dataInicial") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
-    												 @RequestParam("dataFinal") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal) {
-        return transferenciaService.obterTransacoesPorData(dataInicial, dataFinal);
+    public ResponseEntity<?> obterTransacoesPorData(
+            @RequestParam(value = "dataInicial", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
+            @RequestParam(value = "dataFinal", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal,
+            @RequestParam(value = "nomeOperadorTransacao", required = false) String nomeOperadorTransacao) {
+        List<Transferencia> transferencias = transferenciaService.obterTransacoesPorFiltros(dataInicial, dataFinal, nomeOperadorTransacao);
+        return ResponseEntity.ok(transferencias);
     }
-    
-   /* @GetMapping
-    public List<Transferencia>
-    */
 }
+
